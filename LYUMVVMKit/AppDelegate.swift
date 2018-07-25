26e616d5a125fbd8b99b11dc63167aa37a4b5417
吕陈强 <lvchenqiang@ellabook.cn>
2018-07-25 18:32:10 +0800

    屏幕旋转方向

--- LYUMVVMKit/AppDelegate.swift
+++ LYUMVVMKit/AppDelegate.swift
@@ -51,5 +51,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .landscape;
+    }
 }
 
