98f1f24664916492a8808fa4cf32cb7dfcd1d31f
吕陈强 <lvchenqiang@ellabook.cn>
2018-08-15 23:02:20 +0800

    Add test

--- LYUMVVMKit/Module/Message/MessageVC.swift
+++ LYUMVVMKit/Module/Message/MessageVC.swift
@@ -12,13 +12,25 @@ class MessageVC: BaseViewController {
 
 
     override func viewDidLoad() {
-        super.viewDidLoad() 
+        super.viewDidLoad()
+        let lab = UILabel()
+        lab.frame = CGRect(x: 100, y: 100, width: 100, height: 100);
+        lab.backgroundColor = UIColor.gray;
+        lab.layer.cornerRadius = 10;
+        lab.layer.masksToBounds = false;
+        lab.layer.shadowRadius = 10;
+        lab.layer.shadowColor = UIColor.red.cgColor;
+        lab.layer.shadowOffset = CGSize(width: -5, height: -5)
+        lab.layer.shadowOpacity = 1;
+        
+        self.view.addSubview(lab)
+        
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         
-        LYURouter.open(vc: LoginVC());
+//        LYURouter.open(vc: LoginVC());
         
     }
     override func didReceiveMemoryWarning() {
